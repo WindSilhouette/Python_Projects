b04e5a362ae1da0642c907a0903f5bb391153381
WindSilhouette <144385674+WindSilhouette@users.noreply.github.com>
2023-09-10 09:22:13 +0400

    Add files via upload

---
+++ #Python Begginner notes.py
@@ -0,0 +1,80 @@
+import random
+#Python Begginner notes
+# #1:Variables
+# player_choice = "rock"
+# computer_choice = "paper"
+
+# #2: Functions Rock Paper Scissors #6: If statements
+# def get_choices():
+#     options  = ['rock','paper','scissors']
+#     player_choice = input("Enter ")
+#     computer_choice = random.choice(options)
+#     choices = {"player": player_choice, "computer": computer_choice}
+
+#     return choices
+# def check_win(x,y):
+#     print(f"choise {x},  choise {y}") 
+#     if x == y:
+#         return "Tie"
+#     elif x == "rock":
+#         if y == 'scissors':
+#             return "x won"
+#         else:
+#             return "y won"
+#     elif x == "paper":
+#         if y == 'rock':
+#             return "x won"
+#         else:
+#             return "y won"
+#     elif x == "scissors":
+#         if y == 'paper':
+#             return "x won"
+#         else:
+#             return "y won"
+
+# choices = get_choices()
+# result = check_win(choices["player"], choices["computer"])
+# print(result)
+        
+# #3: Calling Function 
+# get_choices()
+
+# #4 Dictionaries
+# dict = {"name": "beau", "color": choices}
+# Accessing dictionary values 
+# value = dict['name']
+
+# #5: Lists
+# food = ["pizza", 2,4] (lists)
+# dinner = random.choice(food) chooses random element from list
+
+# 8 Data Types: String, Integer, Boolean, sequence, set, mapping data: dict, binary,  
+#check data type with isinstance(variable to check, the data type)
+
+# #9 operators 
+# == equal
+# != not equal
+# > greater
+# <= less than 
+# ** astichan 
+# // bajani arandz mnacord
+# % qani hat dranic mejy
+#Boolean operators
+# and &
+# not |
+# or ^
+# NOT ~
+# << SHIFT LEFT 
+# >> SHIFT RIGHT
+# is checks if two object are the same object
+# in checks if object is in the list or another sequence
+#ternary operator  helps to define a condition ex. return True if age > 18 else False
+
+# #10: String methods
+# .upper makes all capital letters 
+# .lower makes all capital letters
+# .title makes first letter of each sting capital
+# .islower checks if all letters are lower 
+# .isalpha contains characters
+# etc check String methods in Youtube video
+
